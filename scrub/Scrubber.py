0f13330cdea6a894cd57b6a004ade7156115f3c1
RajagopalSrinivasan <rajagopal.s@flipkart.com>
2018-04-13 10:58:35 +0530

    Removing unneccesary files
    Following files were removed:
    1. ConfigManager - Replaced by GlobalConfigManager
    2. scrub_user, scrub_bucket_boto, scrub_bucket_rados - Replaced by scrub.py
    3. test scripts
    4. help files

--- scrub/Scrubber.py
+++ scrub/Scrubber.py
@@ -3,10 +3,6 @@ from abc import ABCMeta, abstractmethod
 
 class Scrubber():
     __metaclass__ = ABCMeta
-    #
-    # @abstractmethod
-    # def __init__(self, cluster, bucket_name, num_threads, output_file_name, log_file, show_progress_bar=True):
-    #     pass
 
     @abstractmethod
     def has_completed(self):
--- scrub/test_connection.py
+++
@@ -1,7 +0,0 @@
-import rados
-try:
-	cluster = rados.Rados(conffile='/etc/rados_test/prod-d42sa/ceph.conf')
-	cluster.connect()
-	print "\nCluster ID: " + cluster.get_fsid()
-except Exception as ex:
-	print str(ex)
